8a48006b1fe653875a1eaed97b0ce5ba1fa4e236
Punnakan Punsiri <punnakan2509@gmail.com>
2024-09-11 23:33:26 +0700

    v.1.1

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("asdasdadws")
+const navitems_menu = document.getElementById("ham-menu");
+const navMenu = document.getElementById("navMenu");
+
+navitems_menu.addEventListener("toggle", toggleMenu());
+function toggleMenu() {
+  navMenu.classList.toggle(".nav-items-container.active");
+}
